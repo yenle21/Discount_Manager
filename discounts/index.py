51765f4e03990952b92bc63cbcc16c8ae988e3de
yenlele21 <leyen6885@gmail.com>
2026-04-02 13:38:58 +0700

    update

--- discounts/index.py
+++ discounts/index.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
+
+from discounts import dao
+from discounts.dao import load_products, load_categories
 
 app = Flask(__name__)
 
 # Đọc dữ liệu từ JSON
-def load_products():
-    with open("data/sanpham.json", encoding="utf-8") as f:
-        return json.load(f)
-
 @app.route("/")
-def home():
-    products = load_products()
-    return render_template("customer.html", products=products)
+def customer():
+    cate_id = request.args.get("cate_id", type=int)  # lấy từ URL
+    products = dao.load_products(cate_id=cate_id)
 
+    categories = dao.load_categories()
+
+    return render_template(
+        "customer.html",
+        products=products,
+        categories=categories,
+        cate_id=cate_id,
+    )
 if __name__ == "__main__":
     app.run(debug=True)
