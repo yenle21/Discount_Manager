54f126558b566dfc6958fe3f0cdbc8097e732360
nhn-y <nhuy06022005@gmail.com>
2026-04-03 14:28:58 +0700

    Update Login and Register

--- discounts/dao.py
+++ discounts/dao.py
@@ -1,4 +1,8 @@
-from models import Category, Product
+from werkzeug.security import generate_password_hash, check_password_hash
+import email
+from discounts.models import User, KhachHang, Category, Product
+
+#from models import Category, Product
 from discounts import db
 
 def load_categories():
@@ -21,3 +25,37 @@ def load_products(cate_id=None, kw=None):
 def get_product_by_id(product_id):
     """Lấy chi tiết một sản phẩm theo ID"""
     return db.session.get(Product, product_id)
+
+
+#NhuY
+def get_user_by_id(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except (ValueError, TypeError):
+        return None
+
+def add_user(name, username, password, avatar=None, email=None, sdt=None):
+    password = generate_password_hash(password.strip())
+
+    u = KhachHang(name=name,
+                  username=username.strip(),
+                  password=password,
+                  avatar=avatar,
+                  email=email,
+                  sdt=sdt,
+                  user_role=0)
+
+    db.session.add(u)
+    db.session.commit()
+    return u
+
+def check_username_exists(username):
+    return User.query.filter(User.username == username.strip()).first() is not None
+
+def auth_user(role, username, password):
+    user = User.query.filter(User.user_role == int(role), User.username == username.strip()).first()
+
+    if user and check_password_hash(user.password, password.strip()):
+        return user
+
+    return None
--- discounts/index.py
+++ discounts/index.py
@@ -1,7 +1,13 @@
-
+import re
+import cloudinary
+import cloudinary.uploader
+from flask import redirect, flash, url_for
 from flask import Flask, render_template
-from flask_login import LoginManager, current_user
-from discounts import app
+from flask_login import LoginManager, current_user, login_user, logout_user
+
+from discounts import app, db, login
+from discounts.models import UserRole
+
 import json
 
 
@@ -41,12 +47,80 @@ def admin():
 @app.route("/create")
 def create():
     return render_template("admin/create_voucher.html")
-@app.route("/login")
-def login():
-    return render_template("/login.html")
-@app.route("/register")
+
+
+#NhuY Login, Register
+@app.route("/login", methods=['get', 'post'])
+def user_login():
+    if current_user.is_authenticated:
+        if int(current_user.user_role) == 1:
+            return redirect('/admin')
+        return render_template('customer/customer.html')
+
+    err_msg = None
+    if request.method.__eq__('POST'):
+        role = request.form.get("role")
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = dao.auth_user(role, username, password)
+
+        if user:
+            login_user(user)
+            if int(user.user_role) == 1:
+                return redirect('/admin')
+            return redirect('/')
+        else:
+            err_msg = "Tài khoản hoặc mật khẩu không đúng!"
+            return render_template("login.html", err_msg=err_msg)
+    return render_template("login.html")
+
+@app.route("/register",  methods=['get', 'post'])
 def register():
-    return render_template("/register.html")
+    err_msg = None
+
+    if request.method.__eq__("POST"):
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+        email = request.form.get('email')
+
+        password_pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+        # import pdb #kiểm tra lỗi
+        # pdb.set_trace()
+
+        if dao.check_username_exists(username):
+            err_msg = "Tên đăng nhập này đã tồn tại! Vui lòng chọn tên khác."
+        elif not re.match(password_pattern, password):
+            err_msg = ("Mật khẩu phải có ít nhất 8 ký tự. Gồm chữ hoa, chữ thường, số, ký tự đặc biệt.")
+        elif not password.__eq__(confirm):
+            err_msg = "Mật khẩu không khớp!"
+        elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            err_msg = "Định dạng email không hợp lệ!"
+        else:
+            name = request.form.get("name")
+            username = request.form.get("username")
+            avatar = request.files.get("avatar")
+            path_file = None
+            if avatar:
+                res = cloudinary.uploader.upload(avatar)
+                path_file = res["secure_url"]
+            try:
+                dao.add_user(name, username, password, email, avatar=path_file)
+                return redirect('/login')
+            except:
+                db.session.rollback()
+                err_msg = "Hệ thống đang có lỗi! Vui lòng quay lại sau!"
+    return render_template("register.html", err_msg=err_msg)
+
+@login.user_loader
+def get_user(user_id):
+    return dao.get_user_by_id(int(user_id))
+
+@app.route('/logout')
+def user_logout():
+    logout_user()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
