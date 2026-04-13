20f715cbcafa3bae673100fdbb6d7cf9ae05d359
NguyenNhu-gif <nhu.nt2508@gmail.com>
2026-04-14 03:56:39 +0700

    Test case cho homepage_customer

--- discounts/test/test_base.py
+++ discounts/test/test_base.py
@@ -1,14 +1,15 @@
 import pytest
 from flask import Flask
-
 from discounts import db
+from discounts.models import Product, Category   # thêm dòng này
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
     app.config["TESTING"] = True
-    app.secret_key =  '^#@$*Juifdyfuhsfai#@&#^*'
+    app.secret_key = '^#@$*Juifdyfuhsfai#@&#^*'
+    app.config['CART_KEY'] = 'cart'
     db.init_app(app)
 
     from discounts.index import register_routes
@@ -16,10 +17,8 @@ def create_app():
 
     return app
 
-@pytest.fixture
-def test_client(test_app):
-    return test_app.test_client()
 
+# 🔹 App fixture
 @pytest.fixture
 def test_app():
     app = create_app()
@@ -30,28 +29,31 @@ def test_app():
         db.drop_all()
 
 
+# 🔹 Client
+@pytest.fixture
+def test_client(test_app):
+    return test_app.test_client()
+
+
+# 🔹 DB session
 @pytest.fixture
 def test_session(test_app):
-    yield  db.session
-    db.session.rollback()
+    with test_app.app_context():
+        yield db.session
+        db.session.rollback()
 
 
-@pytest.fixture
-def sample_product(test_session):
-    pass
-    # p1 = Product(name= 'iPhone 17',price = 30 , category_id = 1)
-    # p2 = Product(name= 'iPad Pro',price = 20 , category_id = 2)
-    # p3 = Product(name= 'Galaxy S26 Ultra',price = 35 , category_id = 1)
-    # p4 = Product(name= 'iPhone Ultra',price = 35 , category_id = 2)
-
-    # test_session.add_all([p1,p2,p3, p4])
-    # test_session.commit()
-    #
-    # yield [p1, p2, p3, p4]
-
-@pytest.fixture()
-def test_cloudinary(monkeypatch):
-    def fake_upload(file):
-        return{'secure_url':'https://fake-image.png'}
-
-    monkeypatch.setattr('cloudinary.uploader.upload', fake_upload)
+# 🔹 Sample data
+# @pytest.fixture
+# def sample_product(test_session):
+#     cate1 = Category(id=1, name="Sữa")
+#     cate2 = Category(id=2, name="Kem")
+#
+#     p1 = Product(id=1, name="Sữa Vinamilk", price=10, category_id=1)
+#     p2 = Product(id=2, name="Sữa Nuti", price=12, category_id=1)
+#     p3 = Product(id=3, name="Kem Bắp", price=8, category_id=2)
+#
+#     test_session.add_all([cate1, cate2, p1, p2, p3])
+#     test_session.commit()
+#
+#     yield [p1, p2, p3]
